5e1d3066b857c87ea183cd0c6c4619d2180f1642
Franco <francotreboux@gmail.com>
2024-02-25 17:31:13 -0300

    fix fonts error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './ui/global.css'
-import { inter } from './ui/fonts';
+import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
-import { lusitana } from './ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
