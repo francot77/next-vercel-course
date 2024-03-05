38780f81c31af0df83e6e8e35f9705c4a504769b
Franco Treboux <francotreboux@gmail.com>
2024-03-05 15:45:55 -0300

    invoices page with pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,39 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { useSearchParams,useRouter,usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 export default function Search({ placeholder }: { placeholder: string }) {
+
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+
+const handleSearch = useDebouncedCallback((term:string)=>{
+    console.log(`Searching... ${term}`);
+  const params = new URLSearchParams(searchParams)
+  if(term){
+    params.set('query',term)
+  }
+  else{
+    params.delete('query')
+  }
+  replace(`${pathname}?${params.toString()}`)
+},300)
+
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+      defaultValue={searchParams.get('query')?.toString()}
+      onChange={(e)=>{
+        handleSearch(e.target.value)
+      }}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
